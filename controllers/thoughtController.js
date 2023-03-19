dd4f8515c09a7f012e452d2d93d2e979a3109f64
Genevieve Ferrari <genferrari@gmail.com>
2023-03-19 16:52:41 -0700

    updated thought routes and controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -9,7 +9,7 @@ module.exports = {
       .catch((err) => res.status(500).json(err));
   },
   // Get a single thought
-  getSingeThought(req, res) {
+  getSingleThought(req, res) {
     console.log("Here come a thought!");
     Thought.findOne({ _id: req.params.thoughtId })
       .select("-__v")
@@ -67,5 +67,42 @@ module.exports = {
           : res.json(thoughtData)
       )
       .catch((err) => res.status(500).json(err));
+  },
+
+  addReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((thoughtData) => {
+        if (!thoughtData) {
+          return res.status(404).json({ message: "No thought with this id!" });
+        }
+        res.json(thoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // Remove reaction from a thought
+  deleteReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { runValidators: true, new: true }
+    )
+      .then((thoughtData) => {
+        if (!thougthData) {
+          return res.status(404).json({ message: "No thought with this id!" });
+        }
+        res.json(thoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   },
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,6 +5,8 @@ const {
   createThought,
   updateThought,
   deleteThought,
+  addReaction,
+  deleteReaction,
 } = require("../../controllers/thoughtController.js");
 
 // /api/thought
@@ -18,4 +20,9 @@ router
   .put(updateThought)
   .delete(deleteThought);
 
+router.route("/:thoughtId/reactions").post(addReaction);
+
+router.route("/:thougthId/reactionId").delete(deleteReaction);
+
+router.route;
 module.exports = router;
