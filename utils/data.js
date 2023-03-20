40b29d51699b6e8f9814ceac7e731e2c0c16bfb0
Genevieve Ferrari <genferrari@gmail.com>
2023-03-19 20:12:37 -0700

    coded out data

--- utils/data.js
+++ utils/data.js
@@ -1,14 +1,14 @@
-// example data
-{
-  ("username");
-  "lernantino", "email";
-  ("lernantino@gmail.com");
-}
+// // example data
+// {
+//   ("username");
+//   "lernantino", "email";
+//   ("lernantino@gmail.com");
+// }
 
-// example data
-{
-  ("thoughtText");
-  "Here's a cool thought...", "username";
-  "lernantino", "userId";
-  ("5edff358a0fcb779aa7b118b");
-}
+// // example data
+// {
+//   ("thoughtText");
+//   "Here's a cool thought...", "username";
+//   "lernantino", "userId";
+//   ("5edff358a0fcb779aa7b118b");
+// }
