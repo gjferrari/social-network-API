2b94cbb77a6f235748181457ab8ae23e2bc6cc2d
Genevieve Ferrari <genferrari@gmail.com>
2023-03-19 17:20:01 -0700

    added moment

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const moment = require("moment");
 
 const reactionSchema = new Schema({
   reactionId: {
@@ -18,8 +19,8 @@ const reactionSchema = new Schema({
 
   createdAt: {
     type: Date,
-    default: Date.Now,
-    //Use a getter method to format the timestamp on query
+    default: Date.now,
+    get: (date) => moment(date).format("MMM DD, YYYY [at] hh:mm a"),
   },
 });
 const thoughtSchema = new Schema({
