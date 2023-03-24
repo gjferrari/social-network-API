34ad2cfb4ad16cf0838bcfec35c559e9feecc642
Genevieve Ferrari <genferrari@gmail.com>
2023-03-24 11:59:43 -0700

    deleted reaction model and returned reaction schema to Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,29 +1,28 @@
 const { Schema, model } = require("mongoose");
-const reactionSchema = require("./Reaction");
-// const moment = require("moment");
+// const reactionSchema = require("./Reaction");
 
-// const reactionSchema = new Schema({
-//   // reactionId: {
-//   //   type: Schema.Types.ObjectId,
-//   //   default: () => new Types.ObjectId(),
-//   // },
-//   reactionBody: {
-//     type: String,
-//     required: true,
-//     maxLength: 280,
-//   },
+const reactionSchema = new Schema({
+  // reactionId: {
+  //   type: Schema.Types.ObjectId,
+  //   default: () => new Types.ObjectId(),
+  // },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
 
-//   username: {
-//     type: String,
-//     required: true,
-//   },
+  username: {
+    type: String,
+    required: true,
+  },
 
-//   createdAt: {
-//     type: Date,
-//     default: Date.now,
-//     // get: (date) => moment(date).format("MMM DD, YYYY [at] hh:mm a"),
-//   },
-// });
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    // get: (date) => moment(date).format("MMM DD, YYYY [at] hh:mm a"),
+  },
+});
 const thoughtSchema = new Schema({
   thoughtText: {
     type: String,
